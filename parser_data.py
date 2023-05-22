de74476e9d90569cb15906a839a845f7ee89b5be
Iarina Tudor <i.m.tudor@student.tudelft.nl>
2023-05-22 13:26:36 +0200

    add basic critical path analysis

--- parser_data.py
+++ parser_data.py
@@ -1,6 +1,7 @@
+from queue import PriorityQueue
 class Instance:
     def __init__(self, activities: int, horizon: int, and_constraints, or_constraints, bi_constraints, durations,
-                 resource_capacity, resource_use):
+                 resource_capacity, resource_use, est, eft, lst, lft):
         self.activities = activities
         self.horizon = horizon
         self.and_constraints = and_constraints
@@ -9,13 +10,13 @@ class Instance:
         self.durations = durations
         self.resource_capacity = resource_capacity
         self.resource_use = resource_use
-        # self.lst = lst
-        # self.lft = lft
-        # self.eft = eft
-        # self.est = est
+        self.lst = lst
+        self.lft = lft
+        self.eft = eft
+        self.est = est
 
 
-def transform(and_constraints):
+def transform_preds(and_constraints):
     precedences = {}
     for successor, predecessor in and_constraints:
         if successor in precedences:
@@ -24,8 +25,36 @@ def transform(and_constraints):
             precedences[successor] = []
             precedences[successor].append(predecessor)
 
-    print(precedences)
+
     return precedences
+
+
+def transform_succ(and_constraints):
+    precedences = {}
+    for successor, predecessor in and_constraints:
+        if predecessor in precedences:
+            precedences[predecessor].append(successor)
+        else:
+            precedences[predecessor] = []
+            precedences[predecessor].append(successor)
+
+    return precedences
+
+
+def compute_level_bfs(successors):
+    queue = [0]
+    level = {0: 0}
+    while queue:
+        current_task = queue.pop(0)
+        if current_task in successors.keys():
+            for succ in successors[current_task]:
+                if succ not in level.keys():
+                    level[succ] = level[current_task] + 1
+                else:
+                    level[succ] = max(level[succ], level[current_task] + 1)
+                queue.append(succ)
+    print(level)
+    return level
 
 
 # EST of tasks with no predecessors = First logical starting point.
@@ -35,73 +64,60 @@ def transform(and_constraints):
 # EFT of tasks with no predecessors = Estimated task duration.
 # EFT of tasks with predecessors = (Task EST + Estimated task duration).
 # do this before generating or and bi constraints!!!!!!!!
-# def calculate_est_fst_lft(and_constraints, activities, durations):
-#     est = {}
-#     eft = {}
-#     lst = {}
-#     lft = {}
-#
-#     precedences = transform(and_constraints)
-#     # this is a forward pass
-#     queue = []
-#     critical_path = []
-#     successors = []
-#     print("start", and_constraints, "finish")
-#     for i in range(activities):
-#         if i not in precedences.keys():
-#             est[i] = 0
-#             eft[i] = durations[i]
-#
-#             for succ in precedences.keys():
-#                 if i in precedences[succ]:
-#                     queue.append(succ)
-#     while queue:
-#         current_task = queue.pop(0)
-#         preds = []
-#         for pred in precedences[current_task]:
-#             if pred in eft.keys():
-#                 preds.append(pred)
-#         est[current_task] = max([eft[pred] for pred in preds], default= 0)
-#         eft[current_task] = est[current_task] + durations[current_task]
-#
-#         for succ in precedences.keys():
-#             if current_task in precedences[succ]:
-#                 queue.append(succ)
-#
-#     last_task = activities - 1
-#     lft[last_task] = eft[last_task]
-#     lst[last_task] = lft[last_task] - durations[last_task] + 1
-#     for pred in precedences[last_task]:
-#         queue.append(pred)
-#
-#     while queue:
-#         current_task = queue.pop(0)
-#         successors = []
-#         for succ in precedences.keys():
-#             if current_task in precedences[succ] and succ in lst.keys():
-#                 successors.append(succ)
-#         lft[current_task] = min([lst[succ] for succ in successors])
-#         lst[current_task] = lft[current_task] - durations[current_task] + 1
-#         if current_task in precedences.keys():
-#             for pred in precedences[current_task]:
-#                 queue.append(pred)
-#
-#     #
-#     # for task in reversed(critical_path):
-#     #     successors = []
-#     #     for i in precedences.keys():
-#     #         if task in precedences[i]:
-#     #             successors.append(i)
-#     #     if successors:
-#     #         continue
-#     #     lft[task] = max([est[succ] for succ in successors])
-#     #     lst[task] = lft[task] - durations[task]
-#     # print(est)
-#     # print(eft)
-#     # print(lst)
-#     # print(lft)
-#
-#     return est, eft, lst, lft
+def calculate_est_fst_lft(and_constraints, activities, durations):
+    est = {}
+    eft = {}
+    lst = {}
+    lft = {}
+
+    # successor: [predecessors]
+    predecessors = transform_preds(and_constraints)
+
+    # predecessor: [successors]
+    successors = transform_succ(and_constraints)
+
+    level = compute_level_bfs(successors)
+    max_level = max(level)
+    # this is a forward pass
+    queue = PriorityQueue()
+    for i in level.keys():
+        queue.put((level[i], i))
+
+    est[0] = 0
+    eft[0] = 0
+
+    while not queue.empty():
+        priority, current_task = queue.get()
+        if current_task == 0:
+            continue
+        print(current_task)
+        if current_task == 23:
+            print(eft[10])
+            print(eft[18])
+        est[current_task] = max([eft[pred] for pred in predecessors[current_task]])
+        eft[current_task] = est[current_task] + durations[current_task]
+
+
+    for i in level.keys():
+        queue.put((max_level - level[i], i))
+
+    last_task = activities - 1
+    lft[last_task] = eft[last_task] + durations[last_task]
+    lst[last_task] = lft[last_task] - durations[last_task] + 1
+
+    while not queue.empty():
+        priority, current_task = queue.get()
+        if current_task == activities - 1:
+            continue
+        lft[current_task] = min([lst[succ] for succ in successors[current_task]])
+        lst[current_task] = lft[current_task] - durations[current_task] + 1
+
+    print(est)
+    print(eft)
+    print(lst)
+    print(lft)
+
+    return est, eft, lst, lft
 
 
 def generate_or_constraints(k1, k2, activities, precedences):
@@ -159,7 +175,7 @@ def parse_file(filename, k1, k2):
     resource_capacity = [int(r) for r in lines[line].split()]
     f.close()
 
-    # est, eft, lst, lft = calculate_est_fst_lft(precedences, activities, durations)
+    est, eft, lst, lft = calculate_est_fst_lft(precedences, activities, durations)
     # or_constraints = generate_or_constraints(k1, k2, activities, precedences)
     or_constraints = {}
     # this should be done either or bi not at the same time
@@ -175,7 +191,7 @@ def parse_file(filename, k1, k2):
     # horizon = latest_possible_time + 1
 
     return Instance(activities, horizon, and_constraints, or_constraints, bi_constraints, durations, resource_capacity,
-                    resource_use)
+                    resource_use, est, eft, lst, lft)
 
 
 if __name__ == '__main__':
